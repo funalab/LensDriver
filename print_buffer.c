d41eb8a2af164a1ce8d511e7bc3da8023d5b8b43
Akira Funahashi <funa@bio.keio.ac.jp>
2014-04-22 23:16:33 +0900

    Fixed to only display \r and \n when it is at the end of buffer

--- print_buffer.c
+++ print_buffer.c
@@ -1,16 +1,16 @@
 /*
  * Driver for Optotune LensDriver4
  * Author: Akira Funahashi <funa@bio.keio.ac.jp>
- * Last modified: Tue, 18 Jun 2013 01:35:51 +0900
+ * Last modified: Tue, 22 Apr 2014 23:14:06 +0900
  */
 #include "lensdriver.h"
 
 void print_buffer(uint8_t *buf, int nbytes) {
   int i;
   for (i = 0; i < nbytes; i++) {
-    if (buf[i] == '\r') {
+    if (buf[i] == '\r' && i == nbytes - 2) {
       printf("%s\\r%s", RED, DEFAULT);
-    } else if (buf[i] == '\n') {
+    } else if (buf[i] == '\n' && i == nbytes - 1) {
       printf("%s\\n%s", RED, DEFAULT);
     } else if (isalpha(buf[i]) || isdigit(buf[i])) {
       printf("%s%c%s", RED, buf[i], DEFAULT);
