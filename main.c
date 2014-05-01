5549e6b3a9d123682d5cde674568fb398c3cb56b
Akira Funahashi <funa@bio.keio.ac.jp>
2014-05-01 22:41:06 +0900

    Changed sample main.c to set current from 300 to 1300

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /*
  * Driver for Optotune LensDriver4
  * Author: Akira Funahashi <funa@bio.keio.ac.jp>
- * Last modified: Wed, 23 Apr 2014 02:52:02 +0900
+ * Last modified: Thu, 01 May 2014 22:40:40 +0900
  */
 #include "lensdriver.h"
 
 int main(int argc, char* argv[]) {
-  int fd;
+  int i, fd;
   uint32_t mA = 200;   /* 200 mA */
   uint32_t mHz = 1000; /* 1 Hz */
   uint16_t ucurrent = 4000;  /* upper current for sinusoidal */
@@ -23,12 +23,17 @@ int main(int argc, char* argv[]) {
     safe_exit(fd);
   }
   /* Get upper current limit calibration */
-  cmd_get_uppercurrent_calibration(fd);
+  /* cmd_get_uppercurrent_calibration(fd); */
   /* Get lower current limit calibration */
-  cmd_get_lowercurrent_calibration(fd);
+  /* cmd_get_lowercurrent_calibration(fd); */
   /* Get maximum hardware current */
-  cmd_get_maxhardwarecurrent(fd);
+  /* cmd_get_maxhardwarecurrent(fd); */
 
+  for(int i=300; i<=1300; i++) {
+    cmd_set_current_4096(fd, i);
+    /* wait for 100 msec */
+    usleep(100000);
+  }
   /* Set Current in mA (0 to 280mA) */
   /* if (cmd_set_current(fd, mA) == -1) { */
     /* safe_exit(fd); */
@@ -36,10 +41,10 @@ int main(int argc, char* argv[]) {
   /* sleep(2); */
 
   /* Run sinusoidal */
-  if (cmd_run_sinusoidal(fd, ucurrent, lcurrent, mHz) == -1) {
-    safe_exit(fd);
-  }
-  sleep(10);
+  /* if (cmd_run_sinusoidal(fd, ucurrent, lcurrent, mHz) == -1) { */
+    /* safe_exit(fd); */
+  /* } */
+  /* sleep(10); */
 
   /* Reset and close device */
   safe_exit(fd);
